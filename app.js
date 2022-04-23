712a42bef4266f26ef882fd211acd75e1140c58d
Wuncemoor <47093251+Wuncemoor@users.noreply.github.com>
2022-04-23 19:24:53 -0400

    Preparing for heroku deployment

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var app = express();
 
 //mongoose
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://m001-student:m001-mongodb-basics@cluster0.lvigz.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://m001-student:m001-mongodb-basics@cluster0.lvigz.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
